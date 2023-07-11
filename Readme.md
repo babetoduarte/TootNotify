28ee129682594bb40b4684bab302ac48ed8b700b
Jorge A. Duarte <babetoduarte@gmail.com>
2023-07-10 22:11:04 -0500

    Tool can now attach multiple media files, flag content as sensitive, and add content/spoiler warning to post.

--- Readme.md
+++ Readme.md
@@ -15,8 +15,13 @@ options:
                         User who will receive a direct message.
   -m <message_body>, --message <message_body>
                         Body of the message to be sent.
+  -s <spoiler_description>, --spoiler <spoiler_description>
+                        Description for the spoiler warning which hides the message.
+  -f <path_to_media_file [...]> [<path_to_media_file [...]> ...], --files <path_to_media_file [...]> [<path_to_media_file [...]> ...]
+                        List of up to 4 media files to attach to the message.
+  -x, --sensitive       Flag post/media as sensitive content (blur media)
   -v, --verbose         Print out verbose messages
-  --version             Show program's version number and exit
+  --version             show program's version number and exit
 
 ```
 
--- toot_notify.py
+++ toot_notify.py
@@ -2,7 +2,8 @@
 
 from credentials import *
 from mastodon import Mastodon
-from sys import argv, exit
+from sys import argv, exit, stdout
+from time import sleep
 import argparse
 
 
@@ -50,6 +51,25 @@ def main(argv):
         help="Description for the spoiler warning which hides the message.",
     )
 
+    # Argument for a list of attached media (image, video, audio only!)
+    parser.add_argument(
+        "-f",
+        "--files",
+        metavar="<path_to_media_file [...]>",
+        type=str,
+        nargs='+',
+        default=None,
+        help="List of up to 4 media files to attach to the message.",
+    )
+
+    # Argument for flagging post/media as sensitive content
+    parser.add_argument(
+        "-x", "--sensitive",
+        action="store_true",
+        help="Flag post/media as sensitive content (blur media)"
+    )
+
+
     # Argument for verbose output and print-outs
     parser.add_argument(
         "-v", "--verbose",
@@ -69,23 +89,100 @@ def main(argv):
     if args.verbose:
         print(f"ARGV: {args}")
 
+
+    # If only one media file is passed into the script, make sure it gets
+    # converted into a dictionary containing the single path of the media file
+    if isinstance(args.files, str):
+        post_media = [args.files]
+    # Else, the default argparse argument will chain all elements into a list
+    # of strings, containing the paths to the attached files
+    else:
+        post_media = args.files
+
+    # Make sure the number of attached files does not exceed the maximum number
+    # of attached images for Mastodon: maximum 4 images!
+    if not len(post_media) <= 4:
+        print(f"WARNING: the numer of attached media exceeds the maximum allowed per post (4)\n\tOnly adding the first four files!")
+        post_media = post_media[:4]
+
     # If verbose, print the recipient, message and spoiler text
     if args.verbose:
-        print(f"\tRECIPIENT: {args.recipient}\n\tMESSAGE: {args.message}\n\tSPOILER: {args.spoiler}")
+        print(f"\tRECIPIENT: {args.recipient}\n\tMESSAGE: {args.message}\n\tSPOILER: {args.spoiler}\n\tMEDIA: {post_media}\n\tSENSITIVE: {args.sensitive}")
 
-    # Send private toot!
-    # TODO: Add abiliti to attach media!
+    # TODO: Make sure that media attachments follow these rules:
+    # 1. Attached media consists of only images (Maximum 4!), or [DONE]
+    # 2. Attached media consists of only ONE audio file, or
+    # 3. Attached media consists of only ONE video file
     # TODO: Try/catch exceptions, report success/failure before exiting!
-    notify_me(message=args.message, user=args.recipient, spoiler=args.spoiler)
+    send_toot(message=args.message, user=args.recipient, spoiler=args.spoiler,
+              media=add_media(post_media), sensitive=args.sensitive)
 
     # Exit cleanly
     exit(0)
 
-def notify_me(message, user=DEFAULT_RECIPIENT, spoiler=None):
+
+def check_media_upload(media_id_dict):
+    ready = False
+    status = mastodon.media_update(media_id_dict["id"])
+    if status['url'] != None:
+        ready = True
+    return ready
+
+def upload_wait(filename, media_id_dict, seconds=20, max_wait=30):
+    print(f"Uploading file: {filename} with ID: {media_id_dict['id']}")
+    success = False
+    timeout = False
+    elapsed = 1
+    while (not success) and (not timeout):
+        stdout.write("\r")
+        stdout.write("{:2d}s elapsed.".format(elapsed))
+        stdout.flush()
+        if check_media_upload(media_id_dict):
+            print("\nUpload successful!")
+            success = True
+        sleep(1)
+        elapsed += 1
+        if elapsed == max_wait:
+            print("Upload timed out!")
+            timeout = True
+
+    return success
+
+
+def add_file(media_file, description=None, sync=False):
+    media_dict = None
+    try:
+        media_dict = mastodon.media_post(media_file, description=description,
+                                         synchronous=sync)
+    except Exception as e:
+        print(f"ERROR: Could not determine added file's MIME type!\nMastodon only supports image, video, or audio files.\n\tERROR: {e}")
+        print(f"WARNING: Could not add media {media_file}!")
+
+    try:
+       upload_wait(media_file, media_dict)
+    except Exception as e:
+        print(f"ERROR: {e}")
+
+    return media_dict
+
+
+def add_media(media_files):
+    media_ids = []
+    for media_file in media_files:
+        media_ids.append(add_file(media_file))
+
+    if not media_ids:
+        media_ids = None
+
+    return media_ids
+
+def send_toot(message, user=DEFAULT_RECIPIENT, spoiler=None, media=None, sensitive=False):
     status_message = f"{user}\n {message}"
     mastodon.status_post(status=status_message,
                          visibility='direct',
-                         spoiler_text=spoiler)
+                         media_ids=media,
+                         spoiler_text=spoiler,
+                         sensitive=sensitive)
 
 
 if __name__ == '__main__':
